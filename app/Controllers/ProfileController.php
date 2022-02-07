88f172c4648f7fbed19ccbc9dc35e1d4921918ca
unknown <vicelizaga@gmail.com>
2022-02-07 21:28:54 +0800

    First Commit

---
+++ app/Controllers/ProfileController.php
@@ -0,0 +1,15 @@
+<?php
+// app/Controllers/ProfileController.php
+
+namespace App\Controllers;
+use CodeIgniter\Controller;
+
+
+class ProfileController extends Controller
+{
+    public function index()
+    {
+        $session = session();
+        echo "Hello : ".$session->get('name');
+    }
+}
---
+++ app/Models/UserModel.php
@@ -0,0 +1,21 @@
+<?php
+// app/Models/UserModel.php
+
+namespace App\Models;
+use CodeIgniter\Model;
+
+class UserModel extends Model{
+    protected $table = 'users';
+    protected $allowedFields = [
+        'name',
+        'email',
+        'password',
+        'created_at'
+        
+    ];
+}
+class UserModel2 extends Model { 
+ protected $table = 'users'; 
+ protected $primaryKey = 'id'; 
+ protected $allowedFields = ['name', 'email']; 
+} 
